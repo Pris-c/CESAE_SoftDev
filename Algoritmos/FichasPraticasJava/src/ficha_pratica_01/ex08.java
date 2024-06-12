55df81e1b8f809e2ecf1d83962f37c8578497a7c
Priscila <priscila.campoos@yahoo.com.br>
2024-06-12 15:34:46 +0100

    Update ficha_01 ex08

--- Algoritmos/FichasPraticasJava/src/ficha_pratica_01/ex08.java
+++ Algoritmos/FichasPraticasJava/src/ficha_pratica_01/ex08.java
@@ -8,46 +8,35 @@ public class ex08 {
 
         Scanner input = new Scanner(System.in);
 
-        int duracao, h, min, seg;
+        int h, min;
         int somaSeg = 0;
 
         System.out.println("\n-- CALCULA DURAÇÃO DO ALBUM --");
 
         System.out.print("Introduza minutos da música 1: ");
-        duracao = input.nextInt();
-        somaSeg += (duracao * 60);
+        somaSeg += (input.nextInt() * 60);
         System.out.print("Introduza segundos da música 1: ");
-        duracao = input.nextInt();
-        somaSeg += (duracao);
+        somaSeg += (input.nextInt());
 
         System.out.print("Introduza minutos da música 2: ");
-        duracao = input.nextInt();
-        somaSeg += (duracao * 60);
+        somaSeg += (input.nextInt() * 60);
         System.out.print("Introduza segundos da música 2: ");
-        duracao = input.nextInt();
-        somaSeg += (duracao);
+        somaSeg += (input.nextInt());
 
         System.out.print("Introduza minutos da música 3: ");
-        duracao = input.nextInt();
-        somaSeg += (duracao * 60);
+        somaSeg += (input.nextInt() * 60);
         System.out.print("Introduza segundos da música 3: ");
-        duracao = input.nextInt();
-        somaSeg += (duracao);
+        somaSeg += (input.nextInt());
 
         System.out.print("Introduza minutos da música 4: ");
-        duracao = input.nextInt();
-        somaSeg += (duracao * 60);
+        somaSeg += (input.nextInt() * 60);
         System.out.print("Introduza segundos da música 4: ");
-        duracao = input.nextInt();
-        somaSeg += (duracao);
+        somaSeg += (input.nextInt());
 
         System.out.print("Introduza minutos da música 5: ");
-        duracao = input.nextInt();
-        somaSeg += (duracao * 60);
+        somaSeg += (input.nextInt() * 60);
         System.out.print("Introduza segundos da música 5: ");
-        duracao = input.nextInt();
-        somaSeg += (duracao);
-
+        somaSeg += (input.nextInt());
 
         h = somaSeg / (60*60);
         somaSeg = somaSeg % (60*60);
