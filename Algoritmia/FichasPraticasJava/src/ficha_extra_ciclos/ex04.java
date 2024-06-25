a7b3be1b123d429fcee6205268cf1aae522a0f60
Priscila <priscila.campoos@yahoo.com.br>
2024-06-25 20:33:03 +0100

    ficha_extra_ciclos: update 4 e 5

--- Algoritmia/FichasPraticasJava/src/ficha_extra_ciclos/ex04.java
+++ Algoritmia/FichasPraticasJava/src/ficha_extra_ciclos/ex04.java
@@ -1,12 +1,14 @@
 package ficha_extra_ciclos;
 
+import java.util.Scanner;
+
 public class ex04 {
     /*
         Escreva 3 programas que, através de ciclos, exibam os seguintes padrões (4.1, 4.2, 4.3)
      */
     public static void main(String[] args) {
 
-        System.out.println("\n-- PADRÃO 4.1 --");
+        /*System.out.println("\n-- PADRÃO 4.1 --");
         for (int i = 0; i < 4; i++){
             for (int j = 0; j < 10; j++){
                 System.out.print("*");
@@ -33,10 +35,49 @@ public class ex04 {
             for (int k = 0; k < i; k++){
                 System.out.print("*");
             }
+            System.out.println();
+        }*/
+
+        // Resolução com numero de linhas escolhido pelo usuário
+        Scanner input = new Scanner(System.in);
+        int linhas;
+        System.out.println("\n-- CRIAÇÃO DE PADRÕES --");
+        System.out.println("Escolha o número de linhas a ser impressa para cada padrão:");
+        System.out.print(">> ");
+        linhas = input.nextInt();
+
+        System.out.println("\n-- PADRÃO 4.1 --");
+        for (int i = 0; i < linhas; i++){
+            for (int j = 0; j < 10; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        System.out.println("\n-- PADRÃO 4.2 --");
+        for (int i = 1; i <= linhas; i++){
+            for (int j = 0; j < i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        System.out.println("\n-- PADRÃO 4.3 --");
+        int espaco = 0;
+        int contCaracter = 1;
+
+        for (int i = 1; i <= linhas; i++){
+            espaco = (((linhas * 2) - 1) - contCaracter)/2;
+
             for (int j = 0; j < espaco; j++){
                 System.out.print(" ");
             }
+            for (int k = 0; k < contCaracter; k++){
+                System.out.print("*");
+            }
+
             System.out.println();
+            contCaracter += 2;
         }
 
     }
--- Algoritmia/FichasPraticasJava/src/ficha_extra_ciclos/ex05.java
+++ Algoritmia/FichasPraticasJava/src/ficha_extra_ciclos/ex05.java
@@ -1,5 +1,7 @@
 package ficha_extra_ciclos;
 
+import java.util.Scanner;
+
 public class ex05 {
     /*
         Escreva 3 programas que exibam os seguintes padrões (5.1, 5.2, 5.3)
@@ -7,9 +9,8 @@ public class ex05 {
 
     public static void main(String[] args) {
 
-        System.out.println("\n-- PADRÃO 5.1 --");
+/*        System.out.println("\n-- PADRÃO 5.1 --");
         for (int i = 1; i <= 4; i++){
-
             for (int j = 0; j < i; j++){
                 System.out.print(i);
             }
@@ -30,22 +31,26 @@ public class ex05 {
             for (int k = 0; k < i; k++){
                 System.out.print(j);
             }
-            for (int l = 0; l < espaco; l++){
-                System.out.print(" ");
-            }
             System.out.println();
         }
 
 
         System.out.println("\n-- PADRÃO 5.3 --");
+        // maxLinha
+        // maxCaracter
         // i = linha | contador
-        for (int i = 1; i <= 5; i++){
-            espaco = 9 - i;
+        // nCaracter = numero de caracteres impressos
+        // espaco = (maxLinha - nCaracter) / 2
+        // j = contador espaço
+        int maxLinha = 5;
+        int maxCaracter = (linhas * 2) - 1;
+        for (int i = 1, nCaracter = 1; i <= maxLinha; i++, nCaracter += 2){
+            espaco = (maxCaracter - nCaracter) /2;
 
             for (int j = 0; j < espaco; j++){
                 System.out.print(" ");
             }
-
+            // k = caracter a ser impresso
             int k = i;
             while (k > 1){
                 System.out.print(k);
@@ -55,10 +60,80 @@ public class ex05 {
                 System.out.print(k);
                 k++;
             }
-            for (int j = 0; j < espaco; j++){
-                System.out.print(" ");
+            System.out.println();
+        }*/
+
+        // Resolução com numero de linhas escolhido pelo usuário
+        Scanner input = new Scanner(System.in);
+        int linhas, espaco;
+        System.out.println("\n-- CRIAÇÃO DE PADRÕES --");
+        System.out.println("Escolha o número de linhas a ser impressa para cada padrão (1 a 9):");
+        System.out.print(">> ");
+        linhas = input.nextInt();
+        linhas =  linhas < 1 ? 1 : linhas;
+        linhas =  linhas > 9 ? 9 : linhas;
+
+        System.out.println("\n-- PADRÃO 5.1 --");
+        for (int i = 1; i <= linhas; i++){
+
+            for (int j = 0; j < i; j++){
+                System.out.print(i);
             }
             System.out.println();
         }
+
+        System.out.println("\n-- PADRÃO 5.2 --");
+        // linha = maxLinha;
+        // maxCaracter = 1 + ((linha -1) * 2 )
+        // nCaracter = numero de caracteres impressor (+=2)
+        // espacos = (maxCaracter - nCaracter) / 2;
+        // i = contador linhas
+        int maxCaracter;
+        for (int i = 1, nCaracter = 1; i <= linhas; i++, nCaracter+=2){
+            maxCaracter = (linhas*2) -1;
+            espaco = (maxCaracter - nCaracter) / 2;
+            // l = contador espaçoes
+            for (int l = 0; l < espaco; l++){
+                System.out.print(" ");
+            }
+            // k = contador caracter
+            for (int k = 0; k < nCaracter; k++){
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+
+        System.out.println("\n-- PADRÃO 5.3 --");
+        // linhas = maxLinha
+        // maxCaracter
+        // i = linha | contador
+        // nCaracter = numero de caracteres impressos
+        // espaco = (maxCaracter - nCaracter) / 2
+        // j = contador espaço
+        maxCaracter = (linhas * 2) - 1;
+
+        for (int i = 1, nCaracter = 1; i <= linhas; i++, nCaracter += 2){
+            espaco = (maxCaracter - nCaracter) / 2;
+
+            for (int j = 0; j < espaco; j++){
+                System.out.print(" ");
+            }
+            // k = caracter a ser impresso
+            int k = i;
+            while (k > 1){
+                System.out.print(k);
+                k--;
+            }
+            while (k <= i){
+                System.out.print(k);
+                k++;
+            }
+            System.out.println();
+        }
+
+
+
+
+
     }
 }
